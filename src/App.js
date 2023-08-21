c94245abdf78dc4d3fc963fcb5b73487d52d4412
1996svibe <dndwlscjwo87@gmail.com>
2023-08-21 14:06:31 +0900

    aa

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-
-import { useState } from 'react';
-import './App.css';
-import { world } from './data/world';
-import Worldcup from './pages/Worldcup';
-import Login from './pages/Login';
-import MyRouters from './routes/MyRouters';
+import { useState } from "react";
+import "./App.css";
+import { world } from "./data/world";
+import Worldcup from "./pages/Worldcup";
+import Login from "./pages/Login";
+import MyRouters from "./routes/MyRouters";
+import Board from "./pages/Board/Board";
 
 function App() {
-  const [contry] = useState(world)
+  const [contry] = useState(world);
 
   return (
     <div className="App">
       <MyRouters></MyRouters>
-
+      <h1>Feed App</h1>
+      <Board></Board>
     </div>
   );
 }
